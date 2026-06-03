97f2f21d32ac018cb8f475b15bac9e4091921776
elsuperorate <muhrmuhr@gmail.com>
2026-06-03 12:33:48 +0200

    fix: badge font 28pt, circle 34px, properly center number in circle

--- coda-tray.py
+++ coda-tray.py
@@ -128,13 +128,14 @@ def create_icon(count=0):
         badge = str(min(count, 9))
         try:
             bfont = ImageFont.truetype(
-                "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 26)
+                "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 28)
         except Exception:
             bfont = ImageFont.load_default()
-        draw.ellipse([30, 30, 62, 62], fill='#e53935', outline='white', width=2)
+        draw.ellipse([28, 28, 62, 62], fill='#e53935', outline='white', width=2)
         bb = draw.textbbox((0, 0), badge, font=bfont)
-        bw, bh = bb[2] - bb[0], bb[3] - bb[1]
-        draw.text((46 - bw / 2, 46 - bh / 2), badge, fill='white', font=bfont)
+        cx, cy = 45, 45
+        draw.text((cx - (bb[0] + bb[2]) / 2, cy - (bb[1] + bb[3]) / 2),
+                  badge, fill='white', font=bfont)
 
     return img
 
